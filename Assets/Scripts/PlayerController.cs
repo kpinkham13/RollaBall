6b680faef9d3a5e787325f0b04cdb1f66670124a
Kyle Pinkham <kpinkham13@gmail.com>
2024-04-05 21:31:19 -0700

    Final Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public float jumpForce = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+    private int numJumps = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void OnJump ()
+    {
+        if(transform.position.y == 0.5)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            numJumps++;
+        }
+        else if (numJumps == 1)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            numJumps = 0;
+        }
+        
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 7)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerJump : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    private Vector3 playerVelocity;
+    private bool playerGrounded;
+    [SerializeField] private float jumpHeight = 5.0f;
+    private bool jumpPressed = false;
+
+    private float gravityValue = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actualJump();
+    }
+
+    void actualJump()
+    {
+        playerGrounded = _characterController.isGrounded;
+
+        if(playerGrounded)
+        {
+            playerVelocity.y = 0.0f;
+        }
+
+        if(jumpPressed && playerGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * gravityValue);
+            jumpPressed = false;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        _characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void OnJump()
+    {
+        if(_characterController.velocity.y == 0)
+        {
+            jumpPressed = true;
+        }
+    }
+}
